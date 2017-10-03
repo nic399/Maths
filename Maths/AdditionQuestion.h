38a74714a56c2985595a7919904559f4f93284ab
Nicholas Fung <nbfung@sfu.ca>
2017-10-03 12:03:41 -0700

    Add AdditionQuestion class with overridden initilizer

---
+++ Maths/AdditionQuestion.h
@@ -0,0 +1,20 @@
+//
+//  AdditionQuestion.h
+//  Maths
+//
+//  Created by Nicholas Fung on 2017-10-03.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AdditionQuestion : NSObject
+    
+    @property int a;
+    @property int b;
+    
+    -(AdditionQuestion *) init;
+    -(void) printQuestion;
+    -(BOOL) checkAnswer: (int) guess;
+
+@end
---
+++ Maths/AdditionQuestion.m
@@ -0,0 +1,40 @@
+//
+//  AdditionQuestion.m
+//  Maths
+//
+//  Created by Nicholas Fung on 2017-10-03.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import "AdditionQuestion.h"
+
+
+@implementation AdditionQuestion
+    
+    
+    -(AdditionQuestion*)init
+    {
+        self = [super init];
+        if (self)
+        {
+            self.a = arc4random_uniform(1000000);
+            self.b = arc4random_uniform(1000000);
+        }
+        
+        return self;
+        
+    }
+    
+    -(void) printQuestion
+    {
+        
+    }
+    
+    -(BOOL) checkAnswer:(int)guess
+    {
+        
+        return true;
+    }
+    
+    
+    @end
